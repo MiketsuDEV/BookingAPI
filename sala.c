8ade7b85939e1c93b9d0aa30184eff8362934555
Miketsu <miketsudev@gmail.com>
2024-03-04 11:40:39 +0000

    fixed spaguetti code

--- sala.c
+++ sala.c
@@ -2,64 +2,73 @@
 #include <stdlib.h>
 #include "sala.h"
 
-int CAPACIDAD_MAXIMA = 100;
+int CAPACIDAD_MAXIMA = 1000;
+int* ptr_ini_sala; //Puntero fijo al inicio del vector de asientos
+int* ptr_aux;
+int num_asientos;    // tamaño sala de cine
+int num_asientos_ocupados;    // variable que cuenta numero de asientos
+int sala_creada;// 0 = No hay sala creada, 1: Hay una sala creada.
 
-int *ptr;
-
-int n;    // tamaño sala de cine
-
-int id_persona;    // identificador de persona
-
-int id_asiento;    // identificador de asiento
-
-int cont;    // variable que cuenta numero de asientos
-
-int sala_creada;
-
-int comprobar_sala(){sala_creada =0? return -1:return 0;}
+int comprobar_sala(){return sala_creada = 0 ? 0: 1;}
 
 int reserva_asiento(int id_persona){
+  if(!comprobar_sala){return -1;}//Comprobamos sala creada
+  for(ptr_aux = ptr_ini_sala;ptr_aux<=ptr_ini_sala+num_asientos;ptr_aux++){
+    if(*ptr_aux == -1){ *ptr_aux = id_persona; return 0;}
+  }
+  return -1;
 }
 
-int libera_asiento (int id_asiento){*(ptr+id_asiento) = -1);}
+int libera_asiento (int id_asiento)
+{
+  if(!comprobar_sala) return -1;
+  *(ptr_ini_sala+id_asiento) = -1;
+}
 
-int estado_asiento (int id_asiento){
-  if (id_asiento > n){
+int estado_asiento (int id_asiento)
+{
+  if(!comprobar_sala) return -1;
+  if (id_asiento > capacidad_sala || id_asiento < -1){
     return -1;
   }
-  if (*(ptr + id_asiento) == -1){
+
+  if (*(ptr_ini_sala + id_asiento) == -1){
     return 0;
   }else{
-    return id_persona;
+    //return *(ptr_ini_sala + 
   }
 
 }
 
-int asientos_libres (){return CAPACIDAD_MAXIMA - cont;}
+int asientos_libres (){return CAPACIDAD_MAXIMA - num_asientos_ocupados;}
 
-int asientos_ocupados(){return cont;}
+int asientos_ocupados(){return num_asientos_ocupados;}
 
-int capacidad_sala(){return n;}
+int capacidad_sala(){return capacidad_sala;}
 
-int crear_sala (int capacidad){
-  printf("Selecciona un tamaño para la sala de cine");
-  scanf ("%d",&n);
-  if (n>CAPACIDAD_MAXIMA){
-    printf("La capacidad del Cine no puede excedere CAPACIDAD_MAXIMA");
-    n = 0;
-    return -1;
+int crear_sala (int capacidad)
+{
+  if (!comprobar_sala()) return -1; //Sala ya creada
+
+  num_asientos = capacidad;
+  if(capacidad>CAPACIDAD_MAXIMA || capacidad <= 0)return -1;//Comprobamos que la capacidad esta dentro del rango permitido
+
+  ptr_ini_sala = (int*) malloc(capacidad * sizeof(int));//Reservamos memoria
+  if(ptr_ini_sala == NULL) return -1;//Comprobamos que se ha reservado memoria
+  
+  for (int i =1;i<=capacidad;i++){//Ini todos los asientos vacios (-1)
+    *(ptr_ini_sala + i) = -1;
   }
-  ptr = (int*) malloc(n * sizeof(int));
-
-  for (int i =1;i<=n;i++){
-    *(ptr + i) = -1;
-  }
+  sala_creada = 1;
   return 0;
 
 
 }
-int eliminar_sala(){
-  free(ptr);
-  n = 0;
+int eliminar_sala()
+{
+  if(comprobar_sala) return -1;
+  free(ptr_ini_sala);
+  num_asientos = 0;
+  sala_creada = 0;
   return 0;
 }
