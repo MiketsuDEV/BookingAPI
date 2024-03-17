b979eb89164d408da59912c8bbb3bc8b498f767b
Miketsu <miketsudev@gmail.com>
2024-03-17 17:33:12 +0000

    fixed: libera asiento. added: new tests

--- test_sala.c
+++ test_sala.c
@@ -29,7 +29,7 @@ void FIN_TEST (const char* titulo_test)
   printf ("********** hecho\n");
 }
 
-void test_ReservaBasica()
+void test_reserva_basica()
 {
 	int mi_asiento;
 	INICIO_TEST("Reserva básica");
@@ -44,7 +44,7 @@ void test_ReservaBasica()
 	FIN_TEST("Reserva básica");
 }
 
-void test_IntegridadSalaCreada()
+void test_integridad_sala_creada()
 {
 	INICIO_TEST("Integridad Sala Creada");
 	DebeSerCierto(reserva_asiento(ID_4) == -1);
@@ -68,11 +68,77 @@ void test_IntegridadSalaCreada()
 	FIN_TEST("Integridad Sala Creada");
 
 }
+void test_reserva_multiple()
+{
+	INICIO_TEST("Reserva Multiple");
+	DebeSerCierto(crea_sala(CAPACIDAD_PEREZ_GALDOS) == CAPACIDAD_PEREZ_GALDOS);
+	sentarse(ID_1);
+	sentarse(-15);
+	sentarse(ID_4);
+	sentarse(ID_3);
+	levantarse(1);
+	levantarse(15);
+	levantarse(-130);
+	levantarse(3);
+	estado_sala();
+	DebeSerCierto(elimina_sala() == 0);
+	FIN_TEST("Reserva Multiple");
+}
+void estado_sala()
+{
+	printf("\n######################################\n");
+	printf("Estado Sala: ");
+	capacidad_sala() == 0 ? printf("cerrada.\n") : printf("abierta.\n");
+	printf("Aforo: %d.\n", capacidad_sala());
+	printf("Asientos ocupados: %d.\n", asientos_ocupados());
+	printf("Asientos libres: %d.\n", asientos_libres());
+	char asiento;
+	for(int i=1; i<=capacidad_sala(); i++)
+	{
+		asiento = estado_asiento(i) <= 0 ? ' ' : 'x';  
+		printf("[%c]", asiento);
+		if(i % 30 == 0) printf("\n");
+	}
+	printf("\n######################################\n");
+	fflush(stdout);
+}
+void sentarse(int id_persona)
+{
+	int id_asiento = reserva_asiento(id_persona);
+	switch (id_asiento)
+	{
+	case -1:
+		printf("Error al intentar reservar el asiento.\n");
+		break;
+	
+	default:
+		printf("Se ha reservado el asiento %d.\n", id_asiento);
+		break;
+	}
+}
+void levantarse(int id_asiento)
+{
+	int id_persona = libera_asiento(id_asiento);
+	switch (id_persona)
+	{
+	case 0:
+		printf("El asiento ya esta libre.\n");
+		break;
+	case -1:
+		printf("Error al intentar liberar asiento.\n");
+		break;
+	default:
+		printf("Se ha liberado el asiento %d.\n", id_asiento);
+		break;
+	}
+}
 void ejecuta_tests ()
 {
-	test_IntegridadSalaCreada();
-	test_ReservaBasica();
-	test_IntegridadSalaCreada();
+	test_integridad_sala_creada();
+	test_reserva_basica();
+	test_integridad_sala_creada();
+	test_reserva_multiple();
+	test_integridad_sala_creada();
 }
 
 
--- test_sala.h
+++ test_sala.h
@@ -2,7 +2,11 @@
 #define TEST_SALA_H
 void INICIO_TEST (const char* titulo_test);
 void FIN_TEST (const char* titulo_test);
-void test_ReservaBasica();
-void test_IntegridadSalaCreada();
+void test_reserva_basica();
+void test_integridad_sala_creada();
+void test_reserva_multiple();
+void estado_sala();
+void sentarse(int id_persona);
+void levantarse(int id_asiento);
 void ejecuta_tests();
 #endif
