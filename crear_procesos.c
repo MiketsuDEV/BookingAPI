af17cd93141a86dea903521821320ec433087da2
Alejo <alealejo44@gmail.com>
2024-03-20 12:07:31 +0000

    i have added main programm of crear_procesos.c, but we need to create a procces with fork and exec() to open the gnome-terminal

--- crear_procesos.c
+++ crear_procesos.c
@@ -1,1 +1,35 @@
 #include "crear_procesos.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+void crear_sucursal(const *char ciudad, capacidad);
+void crear_sucursal(const *char ciudad, capacidad){
+// aqui hacemos la llamada a minishell con creacion de procesos con fork y exec
+	int pid_t = fork();
+	
+}
+
+
+
+
+main(){
+	int capacidad;
+	char nombresala;
+
+	while(1){
+		// pedimos nombre y cantidad de la sala
+		printf("Dime cual quieres que sea el tamaño de la sala");
+
+		printf("dime el nombre que quieres ponerle a la sala");
+
+
+		if(!strcmp("salir",nombresala))break;	// aqui lo que hace es que si salir es el nombre de 
+							// la sala pues acaba el programa y sale del shell
+
+		void crear_sucursal(nombresala,capacidad);
+	}
+
+
+}
--- crear_procesos.h
+++ crear_procesos.h
@@ -1,5 +1,9 @@
 #ifndef CREAR_PROCESOS_H
 #define CREAR_PROCESOS_H
 
+void crear_sucursal(const *char ciudad, int capacidad);
+// creamos la sala en la ciudad que nos pidan con cierta capacidad
+void crear_sucursal (char nombresala, int capacidad);
+// creamos sala con el nombre que se escriba por pantalla
 
 #endif
