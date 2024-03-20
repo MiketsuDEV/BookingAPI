2457767d4f96991eca7e107e9075d068b561aa72
Alejo <alealejo44@gmail.com>
2024-03-20 21:23:35 +0000

    error terminal soved

--- crear_procesos.h
+++ crear_procesos.h
@@ -1,7 +1,7 @@
 #ifndef CREAR_PROCESOS_H
 #define CREAR_PROCESOS_H
 
-void crear_sucursal(const *char ciudad, int capacidad);
+void crear_sucursal(const char *ciudad, int capacidad);
 // creamos la sala en la ciudad que nos pidan con cierta capacidad
 
 
