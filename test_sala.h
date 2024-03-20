bd613b1eba085ef4789915ebecd8ae98d8104cf4
Miketsu <miketsudev@gmail.com>
2024-03-20 10:52:18 +0000

    added: 2 new files

--- test_sala.h
+++ test_sala.h
@@ -1,14 +1,14 @@
 #ifndef TEST_SALA_H
 #define TEST_SALA_H
-void INICIO_TEST (const char* titulo_test);
-void FIN_TEST (const char* titulo_test);
+void INICIO_TEST(const char *titulo_test);
+void FIN_TEST(const char *titulo_test);
 void test_reserva_basica();
 void test_integridad_sala_creada();
 void test_reserva_multiple();
 void estado_sala();
 void sentarse(int id_persona);
 void levantarse(int id_asiento);
-void reserva_multiple(int npersonas, int* lista_id);
-void liberar_multiple(int nasientos, int* lista_id_asiento);
+void reserva_multiple(int npersonas, int *lista_id);
+void liberar_multiple(int nasientos, int *lista_id_asiento);
 void ejecuta_tests();
 #endif
