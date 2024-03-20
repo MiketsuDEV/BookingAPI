6f3a99b8ae0d9cf6ac5d28382adc45d1e286575e
Miketsu <miketsudev@gmail.com>
2024-03-20 22:27:31 +0000

    added: new commands

--- shell_interactivo.c
+++ shell_interactivo.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "shell_interactivo.h"
 #include "crear_procesos.h"
+#include "sala.h"
 
 #define bool int
 #define false 0
@@ -23,19 +24,72 @@ int procesar_input(char* input)
 {
     input[strcspn(input, "\n")] = 0; // quitar el salto de linea
     printf("DEBUG INPUT: %s\n", input);
-    if(!strcmp(input, "salir"))
-    {
-        return true;
-    }
+    if(!strcmp(input, "salir")) return true;
+    
+    //procesamos el comando a realizar
     char* comando = strtok(input, " ");
     if(!strcmp(comando, "crear_sala"))
     {
-        char* nombre_sala = strtok(NULL, " ");
-        int capacidad_sala = atoi(strtok(NULL, " "));
-        printf("DEBUG Nombre de la sala: %s\n", nombre_sala);
-        printf("DEBUG Capacidad de la sala: %d\n", capacidad_sala);
-        crear_sucursal(nombre_sala, capacidad_sala);
+        procesar_crear_sala();
+        return false;
+
+    }else if(!strcmp(comando, "reserva"))
+    {
+        procesar_reservar_asiento();
+        return false;
+
+    }else if(!strcmp(comando, "libera"))
+    {
+        procesar_liberar_asiento();
+        return false;
+    }else if(!strcmp(comando, "estado_sala"))
+    {
+        procesar_estado_sala();
         return false;
     }
     return false;
+}
+int procesar_crear_sala()
+{
+    char* nombre_sala = strtok(NULL, " ");
+    int capacidad_sala = atoi(strtok(NULL, " "));
+    printf("DEBUG Nombre de la sala: %s\n", nombre_sala);
+    printf("DEBUG Capacidad de la sala: %d\n", capacidad_sala);
+    //crear_sucursal(nombre_sala, capacidad_sala);
+    crea_sala(capacidad_sala);
+    return 0;
+}
+int procesar_reservar_asiento()
+{
+    int id_persona = atoi(strtok(NULL, " "));
+    reserva_asiento(id_persona);
+    printf("DEBUB Reserva para el id_persona: %d\n", id_persona);
+    return 0;
+}
+int procesar_liberar_asiento()
+{
+    int id_persona = atoi(strtok(NULL, " "));
+    for(int i=1; i<=capacidad_sala(); i++)
+	{
+		if(estado_asiento(i) == id_persona){libera_asiento(i);return 0;}
+	}
+    return 1;
+}
+int procesar_estado_sala()
+{
+	printf("\n######################################\n");
+	printf("Estado Sala: ");
+	capacidad_sala() == 0 ? printf("cerrada.\n") : printf("abierta.\n");
+	printf("Aforo: %d.\n", capacidad_sala());
+	printf("Asientos ocupados: %d.\n", asientos_ocupados());
+	printf("Asientos libres: %d.\n", asientos_libres());
+	char asiento;
+	for(int i=1; i<=capacidad_sala(); i++)
+	{
+		asiento = estado_asiento(i) <= 0 ? ' ' : 'x';  
+		printf("[%c]", asiento);
+		if(i % 30 == 0) printf("\n");
+	}
+	printf("\n######################################\n");
+	fflush(stdout);
 }
--- shell_interactivo.h
+++ shell_interactivo.h
@@ -2,5 +2,8 @@
 #define SHELL_INTERACTIVO_H
 void leer_shell();
 int procesar_input(char* input);
-
+int procesar_crear_sala();
+int procesar_reservar_asiento();
+int procesar_liberar_asiento();
+int procesar_estado_sala();
 #endif
