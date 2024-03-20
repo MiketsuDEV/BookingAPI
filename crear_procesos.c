d2159e75d416fac6a98a33539fe38d619d403a78
Alejo <alealejo44@gmail.com>
2024-03-20 12:20:11 +0000

    added 2 lines where i catch nombresala and capacidad from the keyboard

--- crear_procesos.c
+++ crear_procesos.c
@@ -16,20 +16,21 @@ void crear_sucursal(const *char ciudad, capacidad){
 
 main(){
 	int capacidad;
-	char nombresala;
+	char nombresala[20];
 
 	while(1){
 		// pedimos nombre y cantidad de la sala
 		printf("Dime cual quieres que sea el tamaño de la sala");
-
+		scanf("%d",capacidad);
 		printf("dime el nombre que quieres ponerle a la sala");
+		scanf("%s",nombresala);
 
 
 		if(!strcmp("salir",nombresala))break;	// aqui lo que hace es que si salir es el nombre de 
 							// la sala pues acaba el programa y sale del shell
 
 		void crear_sucursal(nombresala,capacidad);
-	}
+	}// aqui acaba el programa
 
 
 }
