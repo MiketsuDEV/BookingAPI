ad47d8c4bad9d62f5ff74dc2db83d12bc890c9bc
Alejo <alealejo44@gmail.com>
2024-04-04 23:42:38 +0100

    added child control

--- ejecutable/crear_procesos.c
+++ ejecutable/crear_procesos.c
@@ -6,6 +6,10 @@
 #include "crear_procesos.h"
 #include "../mini_shell/error_manager.h"
 
+pid_t hijos[10];
+
+pid_t result;
+int num_hijos=0;
 
 void crear_sucursal(char *ciudad,  char *capacidad){	// aqui hacemos la llamada a minishell con creacion de procesos con fork y exec
 	int pid_t = fork();
@@ -13,6 +17,21 @@ void crear_sucursal(char *ciudad,  char *capacidad){	// aqui hacemos la llamada 
 		char *tira[] = {"xterm","-e", "../mini_shell/mini_shell", ciudad, capacidad,  NULL};		
 		execvp("xterm",tira);
 		
+	}else{
+		hijos[num_hijos++] = pid_t;
+	}
+
+	for (int i =0;i<num_hijos;i++){
+		int status;
+		result = waitpid(hijos[i],&status,WNOHANG);
+		if(result == -1){
+			perror("error al esperar al hijo");
+		}else if(result >0){
+			if(WIFEXITED(status)){
+				printf("El hijo con el PID %d y con nombre de %d ha terminado con codigo de salida %d \n",hijos[i],ciudad,WIFEXITED(status));
+				hijos[i] = hijos[--num_hijos];
+			}
+		}
 	}
 }
 
