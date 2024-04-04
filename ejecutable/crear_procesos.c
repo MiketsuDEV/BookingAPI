4f4c80993355a38efd704fa2a7a33b3bb3d217ac
Miketsu <miketsudev@gmail.com>
2024-04-04 21:11:05 +0100

    changed: gnome-terminal > xterm

--- ejecutable/crear_procesos.c
+++ ejecutable/crear_procesos.c
@@ -8,8 +8,8 @@
 void crear_sucursal(const char *ciudad,int capacidad){	// aqui hacemos la llamada a minishell con creacion de procesos con fork y exec
 	int pid_t = fork();
 	if (pid_t == 0){	// proceso hijo
-		char *tira[] = {"gnome-terminal","--","./a",NULL};		
-		execvp("gnome-terminal",tira);
+		char *tira[] = {"xterm","-e", "./mini_shell/mini_shell",NULL};		
+		execvp("xterm",tira);
 		
 		
 	}else if(pid_t >1){
@@ -19,7 +19,11 @@ void crear_sucursal(const char *ciudad,int capacidad){	// aqui hacemos la llamad
 	}
 }
 
-
+void main(int argc, char *argv[])
+{
+	crear_sucursal(argv[1], atoi(argv[2]));
+	exit(0);
+}
 
 
 
