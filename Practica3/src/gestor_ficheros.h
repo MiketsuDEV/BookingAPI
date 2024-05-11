0261a1a6c7dc76274912cd031c3cb9b620703c4a
Alejo <alealejo44@gmail.com>
2024-05-11 13:11:36 +0100

    xd

--- Practica3/src/gestor_ficheros.h
+++
@@ -1,9 +0,0 @@
-#ifndef GESTOR_FICHEROS_H
-#define GESTOR_FICHEROS_H
-int guarda_estado_sala(const char* ruta_fichero, bool crea, bool oflag);
-int recupera_estado_sala(const char* ruta_fichero);
-int guarda_estadoparcial_sala(const char* ruta_fichero, int num_asientos, int* id_asientos);
-int recupera_estadoparcial_sala(const char* ruta_fichero, int num_asientos, int* id_asientos);
-int calcular_blk_size(const char* ruta_fichero);
-
-#endif
--- Practica3/src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "sala.h"
-#include "misala.h"
-
-void main(int argc, char *argv[])
-{
-    procesar_orden(argc, argv);
-}
