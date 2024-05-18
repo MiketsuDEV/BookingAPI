3ad49fa852beb6d398db764cb2cbe575847c9f62
Alejo <alealejo44@gmail.com>
2024-05-19 00:36:43 +0100

    saquenme de aqui

--- src/tests.c
+++ src/tests.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "sala.h"
+#include "misala.h"
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
@@ -21,6 +22,7 @@
 #define ID_4 1500
 #define ID_5 2
 #define ID_6 1
+
 
 int* estado_sala(){
 	int *estado = (int*) malloc(capacidad_sala() * sizeof(int));
@@ -48,6 +50,13 @@ void FIN_TEST(const char * titulo_test){
 printf("***************** test terminado\n");
 }
 
+void test_crea(){
+	INICIO_TEST ("test para la creacion de sala");
+	const *char = "../tests/crea",
+	
+	int argc =5;
+	char *argv[] =
+}
 
 
 void test_guarda_estado_sala(){
@@ -61,36 +70,53 @@ void test_guarda_estado_sala(){
 		DebeSerCierto(reserva_asiento(i) == i);
 	}
 	
-	libera_asiento(4);
+	DebeSerCierto(libera_asiento(4)==-15);
 	libera_asiento(8);
+	
 		
 	DebeSerCierto(guarda_estado_sala(ruta_fichero)==0);
-	
-	int * estado = estado_sala();
-	
-	
+	FILE* file = fopen(ruta_fichero, "r");
+	DebeSerCierto(file != NULL);
+	fclose(file);
+	elimina_sala();
 	FIN_TEST("guarda estado");
 }
 
 
 void test_recupera_estado(){
 	INICIO_TEST ("test para recuperar el estado de la sala");
-	
+	crea_sala (100);
 	const char* ruta_fichero = "../tests/guarda_estado_sala";
 	const char * ruta_fichero_falsa = "..test/no_guarda_nada";
+	const char * ruta_fichero_falsa2 = "";
+	DebeSerFalso(recupera_estado_sala(ruta_fichero_falsa2)== 0);
 	DebeSerFalso(recupera_estado_sala(ruta_fichero_falsa)== 0);
 	DebeSerCierto(recupera_estado_sala(ruta_fichero) == 0);
 	DebeSerCierto(estado_asiento(1) ==1);
 	DebeSerCierto(estado_asiento(3) ==3);
 	DebeSerFalso(estado_asiento(4) == 4);
 	DebeSerCierto(asientos_libres() == 92);
+	elimina_sala();
 	FIN_TEST("recupera estado sala");
 }
 
 
 void test_guarda_estado_parcial(){
 	INICIO_TEST ("test para guardar el estado parcial de la sala");
-	char* ruta_fichero = "tests/guarda_estadoparcial_sala";
+	char* ruta_fichero = "../tests/guarda_estado_sala";
+	int capacidad = 100;
+	crea_sala(capacidad);
+	
+	for (int i=1; i<=10; i++){
+		DebeSerCierto(reserva_asiento(i) == i);
+		
+		
+	}
+	int asientos[] = {1,2,3};
+	DebeSerCierto(guarda_estadoparcial_sala(ruta_fichero,3,asientos)==0 );
+	printf("\n valor de estado asiento 1 %d \n", estado_asiento(1));
+	elimina_sala();
+	
 	FIN_TEST("guarda estado parcial");
 }
 
@@ -98,12 +124,32 @@ void test_guarda_estado_parcial(){
 void test_recupera_estado_parcial(){
 
 	INICIO_TEST ("test para recuperar el estado parcial de la sala");
-	char* ruta_fichero = "tests/guarda_estadoparcial_sala";
+	char* ruta_fichero = "../tests/guarda_estado_sala";
+	crea_sala(100);
+	int tamaño = 3;
+	int asientos[] = {1,2,3};
+	DebeSerCierto(recupera_estadoparcial_sala(ruta_fichero,tamaño,asientos) == 0);
+	DebeSerCierto(asientos_ocupados() == 3);
+	printf("\n asientos ocupados %d \n", asientos_ocupados());
+	printf("\n asientos libres %d \n", asientos_libres());
+	for (int i =1; i<=100;i++){
+		printf("\n valor de estado asiento %d %d \n",i, estado_asiento(i));
+
+	}
+	
+	printf("\n valor de estado asiento 10 %d \n", estado_asiento(10));
+	DebeSerCierto(estado_asiento(1) == 1);
+	DebeSerCierto(estado_asiento(2) == 2);
+	DebeSerCierto(estado_asiento(5) != 5);
+	elimina_sala();
+	
 	FIN_TEST("recupera estado parcial");
 }
 
 int main(){
 	test_guarda_estado_sala();
 	test_recupera_estado();
+	test_guarda_estado_parcial();
+	test_recupera_estado_parcial();
 }
 
